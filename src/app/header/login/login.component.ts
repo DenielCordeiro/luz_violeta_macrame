ea4937c33c996765101516d8afd4263053bf9529
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2026-03-22 13:58:05 -0300

    refactor:[Login]: create component start login to open dialog with material design

--- src/app/header/login/login.component.ts
+++ src/app/header/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule,  } from '@angular/forms';
-import { UsersService } from '../../services/users/users.service';
+import { MatDialog } from '@angular/material/dialog';
+import { StartComponent } from './start/start.component';
 
 @Component({
   selector: 'app-login',
@@ -15,8 +16,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private userService: UsersService,
     public route: ActivatedRoute,
+    public dialog: MatDialog,
   ) {}
 
   ngOnInit(): void {
@@ -41,7 +42,9 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  openModal(loginModal: any): void {}
+  loginDialog(): void {
+    this.dialog.open<StartComponent>(StartComponent);
+  }
 
   closeModal(): void {}
 }
---
+++ src/app/header/login/start/start.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-start',
+    standalone: true,
+    imports: [],
+    templateUrl: './start.component.html',
+    styleUrls: ['./start.component.sass']
+})
+export class StartComponent {}
