28be87c688e1b404e66a013993a4477577086961
DenielCordeiro <dcordeiro962@gmail.com>
2026-04-24 14:14:00 -0300

    refactor[Products]: structure HTML to product delete

--- src/app/products/delete-product/delete-product.component.ts
+++ src/app/products/delete-product/delete-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Inject } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { ProductsService } from 'src/app/services/products/products.service';
 import { Product } from 'src/app/interfaces/product.interface';
@@ -9,13 +9,24 @@ import { Product } from 'src/app/interfaces/product.interface';
   templateUrl: './delete-product.component.html',
   styleUrls: ['./delete-product.component.sass'],
 })
-export class DeleteProductComponent {
+export class DeleteProductComponent implements OnInit {
   private productsService: ProductsService = inject(ProductsService);
+  public product!: Product;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public loadedProduct: Product[],
     public dialog: MatDialog
   ) {}
+
+  public ngOnInit(): void {
+    this.product = this.loadProductData();
+  }
+
+  public loadProductData(): Product {
+    const extratedProduct = this.loadedProduct[0];
+
+    return extratedProduct;
+  }
 
   public deletingProduct(productId: string | undefined): void {
     if (productId !== undefined) {
